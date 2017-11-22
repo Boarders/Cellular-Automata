771b5082277470bf14952085b2b0659cf91e52ac
Callan McGill <callanmcgill@Callans-MBP.home>
2017-11-22 10:32:03 +0000

    added Wolfram code rule

--- src/Automata.hs
+++ src/Automata.hs
@@ -28,7 +28,7 @@ instance Functor ListZipper where
   fmap f (L' ls x rs) = L' (fmap f ls) (f x) (fmap f rs)
 
 instance Comonad ListZipper where
-  extract = listRead
+  extract = listZipRead
   duplicate lz@(L' ls a rs) = L' (tail $ iterate leftShift lz) lz (tail $ iterate rightShift lz)
 
 toListIndexes :: Int -> (ListZipper a) -> [(Int,a)]
@@ -45,9 +45,6 @@ toList n (L' ls x rs) = (reverse $ take n' ls) ++ [x] ++ (take n' rs)
 
 toList' :: (ListZipper a) -> [a]
 toList' (L' ls x rs) = (reverse ls) ++ [x] ++ rs
-
-segmentToList :: (ListZipper a) -> Int -> [a]
-segmentToList (L' ls x rs) n = (take n (reverse ls)) ++ [x] ++ (take n rs)
   
 
 leftShift :: ListZipper a -> ListZipper a
@@ -56,8 +53,8 @@ leftShift (L' (l:ls) x rs) = L' ls l (x:rs)
 rightShift :: ListZipper a -> ListZipper a
 rightShift (L' (ls) x (r:rs)) = L' (x:ls) r rs
 
-listRead :: ListZipper a -> a
-listRead (L' _ x _) = x
+listZipRead :: ListZipper a -> a
+listZipRead (L' _ x _) = x
 
 listReassign :: a -> ListZipper a -> ListZipper a
 listReassign x (L' ls _ rs) = L' ls x rs
@@ -65,18 +62,42 @@ listReassign x (L' ls _ rs) = L' ls x rs
 update :: (ListZipper a -> a) -> ListZipper a -> ListZipper a
 update = extend
 
-rule30 :: (ListZipper BW) -> BW
-rule30 (L' ls@(l:ls') x rs@(r:rs'))
-  | (l == Bl && x == Bl && r == Bl) = Wh
-  | (l == Bl && x == Bl && r == Wh) = Wh
-  | (l == Bl && x == Wh && r == Bl) = Wh
-  | (l == Bl && x == Wh && r == Wh) = Bl
-  | (l == Wh && x == Bl && r == Bl) = Bl
-  | (l == Wh && x == Bl && r == Wh) = Bl
-  | (l == Wh && x == Wh && r == Bl) = Bl
-  | (l == Wh && x == Wh && r == Wh) = Wh
+convertTo256Binary' :: Int -> [Int]
+convertTo256Binary' 0 = [0]
+convertTo256Binary' 1 = [1]
+convertTo256Binary' n = (n' `mod` 2) : (convertTo256Binary (n `div` 2))
+  where n' = n `mod` 256
 
+convertTo256Binary = reverse.convertTo256Binary'
+
+code :: Int -> BW
+code 0 = Wh
+code 1 = Bl
+code n = error "Cannot convert non-binary input to black or white." 
+
+wolframCode :: Int -> [BW]
+wolframCode n = map code $  replicate l' 0 ++ ns
+  where ns = convertTo256Binary n
+        l = length ns
+        l'= 8 - l 
+        
+wolframRule :: Int -> (ListZipper BW) -> BW
+wolframRule n (L' ls@(l:ls') x rs@(r:rs'))
+  | (l == Bl && x == Bl && r == Bl) = wolfm !! 0
+  | (l == Bl && x == Bl && r == Wh) = wolfm !! 1
+  | (l == Bl && x == Wh && r == Bl) = wolfm !! 2
+  | (l == Bl && x == Wh && r == Wh) = wolfm !! 3
+  | (l == Wh && x == Bl && r == Bl) = wolfm !! 4
+  | (l == Wh && x == Bl && r == Wh) = wolfm !! 5
+  | (l == Wh && x == Wh && r == Bl) = wolfm !! 6
+  | (l == Wh && x == Wh && r == Wh) = wolfm !! 7
+    where wolfm = wolframCode n
+
+
+rule30 = wolframRule 30
 runRule30 = update rule30
+runRule = update (wolframRule ruleNumber)
+ruleNumber = 110
 
 startState :: ListZipper BW
 startState = L' (repeat Wh) Bl (repeat Wh)
@@ -98,14 +119,10 @@ iterateUpdate :: Int -> M.Map Int BW -> M.Map Int BW
 iterateUpdate 0 state = state
 iterateUpdate n state = iterateUpdate (n-1) newStateMap
   where newStateMap = toMap width newStateZipper
-        newStateZipper = runRule30.stateFromMap $ state
+        newStateZipper = runRule.stateFromMap $ state
 
 
-recurs :: Integer -> Integer
-recurs n
-  | n == 1 = 2
-  | n <= 100 = 2*recurs (n-1)
-  | otherwise = recurs 100
+
 
         
   
